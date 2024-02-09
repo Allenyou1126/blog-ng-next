6e63dfe649b2ead29d9cd438bda3fd8753c6792f
Allenyou <allenyou1126@gmail.com>
2024-02-09 21:57:08 +0800

    修改了config

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -4,12 +4,6 @@ export const config = {
 		title: `秋实-Allenyou的小窝`,
 		description: "稻花香里说丰年，听取WA声一片",
 	},
-	build: {
-		api:
-			process.env.API != undefined
-				? process.env.API
-				: "http://127.0.0.1:5000/api/",
-	},
 	comment: {
 		api: "https://waline.allenyou.wang/",
 	},
