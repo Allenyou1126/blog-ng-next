69914b9cc179deeb6bfc7a638a651398531158db
Allenyou <allenyou1126@gmail.com>
2024-02-11 18:21:31 +0800

    完善了TOC

--- src/components/Toc.tsx
+++ src/components/Toc.tsx
@@ -1,5 +1,4 @@
 import { TocType } from "@/libs/types";
-import Link from "next/link";
 import { ReactNode } from "react";
 import AllenyouLink from "./AllenyouLink";
 
@@ -13,27 +12,31 @@ function rendToc(toc: TocType[]): ReactNode {
 		if (item.child.length === 0) {
 			ret.push(
 				<li key={++index}>
-					<AllenyouLink href={`#${item.id}`}>{item.display}</AllenyouLink>
+					<AllenyouLink className="hover:opacity-80" href={`#${item.id}`}>
+						{item.display}
+					</AllenyouLink>
 				</li>
 			);
 			return;
 		}
 		ret.push(
 			<li key={++index}>
-				<AllenyouLink href={`#${item.id}`}>{item.display}</AllenyouLink>
+				<AllenyouLink className="hover:opacity-80" href={`#${item.id}`}>
+					{item.display}
+				</AllenyouLink>
 				{rendToc(item.child)}
 			</li>
 		);
 	});
-	return <ul className="indent-8">{ret}</ul>;
+	return <ul className="pl-8 w-full opacity-90 font-medium">{ret}</ul>;
 }
 
 export default function Toc({ toc }: { toc: TocType[] }) {
 	const tocContent: ReactNode = rendToc(toc);
 	return (
-		<div className="hidden xl:block absolute left-4 h-full">
-			<div className="h-20"></div>
-			<div className="sticky top-40 w-40 overflow-y-scroll overflow-x-scroll">
+		<div className="hidden 2xl:block absolute left-4 h-full">
+			<div className="h-40"></div>
+			<div className="sticky top-40 w-80 overflow-y-scroll text-wrap">
 				{tocContent}
 			</div>
 		</div>
