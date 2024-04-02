5bbf671c63a527a29c27fe6b77fb19cf2e888c49
Allenyou <allenyou1126@gmail.com>
2024-04-02 15:47:13 +0800

    对暗黑模式代码做了一定精简

--- src/components/DarkModeSwitcher.tsx
+++ src/components/DarkModeSwitcher.tsx
@@ -7,7 +7,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useAtom } from "jotai/react";
-import { useEffect } from "react";
 
 function getDarkModeAlt(mode: "auto" | "light" | "dark") {
 	switch (mode) {
@@ -36,38 +35,16 @@ export default function DarkModeSwitcher() {
 	const handler = () => {
 		switch (theme) {
 			case "auto":
-				localStorage.setItem("dark-mode", "light");
 				setTheme("light");
 				break;
 			case "light":
-				localStorage.setItem("dark-mode", "dark");
 				setTheme("dark");
 				break;
 			case "dark":
-				localStorage.setItem("dark-mode", "follow");
 				setTheme("auto");
 				break;
 		}
 	};
-	useEffect(() => {
-		var local_: string | null = localStorage.getItem("dark-mode");
-		var stat: "light" | "dark" | "auto" = "auto";
-		switch (local_) {
-			case "follow":
-				stat = "auto";
-				break;
-			case "dark":
-				stat = "dark";
-				break;
-			case "light":
-				stat = "light";
-				break;
-			default:
-				localStorage.setItem("dark-mode", "follow");
-				stat = "auto";
-		}
-		setTheme(stat);
-	});
 	return (
 		<button
 			className="text-3xl align-baseline hover:opacity-80 w-8"
--- src/libs/darkMode.ts
+++ src/libs/darkMode.ts
@@ -1,3 +1,6 @@
-import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-export const darkMode = atom<"auto" | "light" | "dark">("auto");
+export const darkMode = atomWithStorage<"auto" | "light" | "dark">(
+	"dark-mode",
+	"auto"
+);
