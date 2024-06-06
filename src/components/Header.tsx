dc19437e4db1c9cd058ad155c42ae4c07b70cced
Allenyou <allenyou1126@gmail.com>
2024-06-06 19:53:59 +0800

    修改了深色模式的细节

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
 	return (
 		<header className="bg-primary bg-furry h-[calc(65lvh+8rem)] min-h-[30rem] bg-cover bg-top flex flex-col z-0">
 			<div className="flex-auto relative">
-				<div className="absolute -translate-x-2/4 left-2/4 bottom-10 w-full max-w-4xl text-white px-5 md:px-6 drop-shadow-xl">
+				<div className="absolute -translate-x-2/4 left-2/4 bottom-10 w-full max-w-4xl text-white px-5 md:px-6 drop-shadow-xl z-10 dark:text-gray-300">
 					<h1 className="text-4xl leading-normal font-bold drop-shadow-xl">
 						<Link href="/">{config.blog.title}</Link>
 					</h1>
@@ -14,6 +14,7 @@ export default function Header() {
 					</p>
 				</div>
 			</div>
+			<div className="fixed hidden dark:block w-full h-full bg-black opacity-50"></div>
 			<div className="flex-none h-32 w-full bg-gradient-to-b from-transparent to-white dark:to-gray-950"></div>
 		</header>
 	);
