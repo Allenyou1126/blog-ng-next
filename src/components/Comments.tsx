8b4d53a1acd28a51a0a89f6d5cf6727deefcf58e
Allenyou <i@allenyou.wang>
2024-09-08 21:30:33 +0800

    为评论系统加载添加了Loading动画

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,21 @@
 "use client";
 import dynamic from "next/dynamic";
+import { Loading } from "./Loading";
 
 export const Comments = dynamic(() => import("@/components/WalineComments"), {
 	ssr: false,
+	loading: () => {
+		return <CommentsLoading />;
+	},
 });
+
+export function CommentsLoading() {
+	return (
+		<>
+			<div className="w-full flex justify-center items-center flex-col gap-4">
+				<Loading />
+				<p className="font-bold text-xl">加载评论中……</p>
+			</div>
+		</>
+	);
+}
